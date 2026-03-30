baa1f602b45ef66e2c11606be4fb75e81dd07627
lgy <lgy123456xyz@gmail.com>
2026-03-30 18:45:59 +0800

    feat: 添加最大值函数及其头文件，更新主程序以调用该函数

---
+++ cpp/cppProject/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "max.h"
+
+int main(){
+    int a=Max(3,2);
+    std::cout<<a<<std::endl;
+}
--- cpp/firstcpp.cpp
+++ cpp/firstcpp.cpp
@@ -1,9 +1,8 @@
 #include<iostream>
 int main(){
     constexpr int a=1;
-    std::cout<<"Hello world"<<std::endl;
+    std::cout<<"Hello world 你好世界"<<std::endl;
     std::cout<<"The value of a is: "<<a<<std::endl;
-    getchar();
     return 0;
 
 }
